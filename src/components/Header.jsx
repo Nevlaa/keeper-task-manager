66e8623200ad2d92cc1c14b12326017492aff96e
Scott horsburgh <shorsburgh19@gmail.com>
2020-12-15 21:09:25 -0700

    adding files

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AssignmentTurnedInIcon />
+        Whats Next Manager
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
